cfb6490f047fdd5b9bb4571a609157809c279f2c
JoePascale <joe.m.pascale@gmail.com>
2017-10-25 12:45:24 -0700

    updated kele.rb to retrieve & store auth token, given valid credentials

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,10 +1,13 @@
+require 'httparty'
+
 class Kele
   include HTTParty
 
   def initialize(email, password)
-    @email = email
-    @password = password
     @url = 'https://www.bloc.io/api/v1'
-    @auth_token =
+    response = self.class.post("#{@url}/sessions",
+      body: {email: email, password: password })
+    @auth_token = response['auth_token']
+    puts response["message"] if response["message"]
   end
 end
