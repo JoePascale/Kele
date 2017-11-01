e4561c2e5534517dcb7b750ab796709275b86048
JoePascale <joe.m.pascale@gmail.com>
2017-10-31 17:02:14 -0700

    updated gem to include query for mentor's available time slots

--- lib/kele.rb
+++ lib/kele.rb
@@ -14,6 +14,15 @@ class Kele
 
   def get_me
     response = self.class.get("#{@url}/users/me", headers: { "authorization" => @auth_token })
-    JSON.parse(response.to_s)
+    JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    @id = mentor_id
+    response = self.class.get("#{@url}/mentors/#{@id}/student_availability", headers: { "authorization" => @auth_token})
+    hash = JSON.parse(response.body)
+    hash.each do |time_slot|
+      p time_slot["starts_at"]
+    end
   end
 end
