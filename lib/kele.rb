3d3d4f1425cf2dd0867487db3d4aa4e25a15dc4b
JoePascale <joe.m.pascale@gmail.com>
2017-11-01 09:19:54 -0700

    added methods to retrieve roadmaps and checkpoints

--- lib/kele.rb
+++ lib/kele.rb
@@ -25,4 +25,16 @@ class Kele
       p time_slot["starts_at"]
     end
   end
+
+  def get_roadmap(roadmap_id)
+    @id = roadmap_id
+    response = self.class.get("#{@url}/roadmaps/#{@id}", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    @id = checkpoint_id
+    response = self.class.get("#{@url}/checkpoints/#{@id}", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
 end
