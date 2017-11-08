68bc270d0d537e67cc6c1fce7e24d1cdcca57f14
JoePascale <joe.m.pascale@gmail.com>
2017-11-08 10:32:07 -0800

    created method for checkpoint submissions

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,7 +9,7 @@ class Kele
   def initialize(email, password)
     @url = 'https://www.bloc.io/api/v1'
     response = self.class.post("#{@url}/sessions",
-      body: {email: email, password: password })
+      body: { email: email, password: password })
     @auth_token = response['auth_token']
     puts response["message"] if response["message"]
   end
@@ -42,8 +42,16 @@ class Kele
   def create_message(sender, recipient_id, token, subject, stripped_text)
     response = self.class.post("#{@url}/messages",
       headers: { "authorization" => @auth_token },
-      body: {sender: sender, recipient_id: recipient_id,
+      body: { sender: sender, recipient_id: recipient_id,
       token: token, subject: subject, stripped_text: stripped_text })
+    puts response["message"] if response["message"]
+  end
+
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    response = self.class.post("#{@url}/checkpoint_submissions",
+    headers: { "authorization" => @auth_token },
+    body: { checkpoint_id: checkpoint_id, assignment_branch: assignment_branch,
+      assignment_commit_link: assignment_commit_link, comment: comment, enrollment_id: enrollment_id })
     puts response["message"] if response["message"]
   end
 end
